3955b4d4d57abfb65f9f76d1e2c29e846c2e9653
Mateusz Bilski <mateusz.bilski@gmail.com>
2017-02-27 13:17:18 +0100

    typo

--- src/main/scala/pl/immutables/circe/model.scala
+++ src/main/scala/pl/immutables/circe/model.scala
@@ -7,12 +7,12 @@ case object Action extends Genre
 case object Comedy extends Genre
 case object Drama extends Genre
 
-case class CricitId(value: String) extends AnyVal
+case class CriticId(value: String) extends AnyVal
 case class UserId(value: String) extends AnyVal
 
 sealed trait Review
 case class RemoteReview(url: Option[String], score: Int) extends Review
-case class CricitReview(cricitId: CricitId, review: String, score: Int) extends Review
+case class CriticReview(cricitId: CriticId, review: String, score: Int) extends Review
 case class UserReview(userId: UserId, review: String, score: Int) extends Review
 
 case class Movie(name: String, genre: Genre, reviews: List[Review], releaseDate: LocalDate)
